285b5a1c3be69a9a4229b6ddcdee2678834920f3
AldebaranAlba <aldebaran.alba0987@gmail.com>
2024-09-21 21:01:51 -0600

    LOL

--- app/src/main/java/com/example/calculadoraactividad/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraactividad/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.calculadoraactividad
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() extends AppCompactActivity implements View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +16,29 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+
+            //Boton
+           Button btn =  (Button) findViewById(@+id/button); //Busca vista
+
+            //Listener para cuando le de click
+            View.OnClickListener listener = new View.OnClickListener(){
+                @override
+                public void onClick(View v){
+                    //Acciones de cuando de clic
+                }
+            };
+
+            //Ejecuta el listener cuando se de el click
+//           btn.setOnClickListener(listener);
+
+            //Con mainActivity
+            btn.setOnClickListener(this);
+
+
     }
+
+        @Override
+        public void onClick(view e){
+
+        }
 }
